c404d2b06b743caf90ac6c837243b94f242157db
Nikhil <nikhilkori0@gmail.com>
2023-01-25 22:15:54 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+alert('Page loaded, JS alert');
+
+function onClick() {
+    alert('I\'m Clicked')
+}
+
+function changeText() {
+    document.getElementById('targetTag').innerHTML= `Hello`;
+}
+
+$(document).ready(function() {
+    $('#hideTitleBtn').click(function() {
+      $('#title').toggle();
+      $('p').text('jQuery text');
+    })
+})
